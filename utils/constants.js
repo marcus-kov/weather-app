f106306069e20524cc2c232ff8e36fbf87fa6621
Marko Kovacevic <marko.kovacevic@rms.com>
2022-07-03 16:49:29 +0200

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const OPEN_WEATHER_API_URL = "https://proxy-cors-weather.herokuapp.com/https://api.openweathermap.org/data/2.5";
 export const GEO_LOCATION_API_URL = "https://api.geoapify.com/v1";
 
 export const OPEN_WEATHER_API_KEY = "6c99b57b652c991f2e8efe771f5e1400";
